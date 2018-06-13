185a6fb86b946ca5d9efa05a3fc15b4b511642b5
james heath <guruvex@gmail.com>
2018-06-13 10:21:13 -0600

    building on to the code

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,11 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,25 +22,31 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
 
-        ArrayList<String> words = new ArrayList<>();
-        words.add("one");
-        words.add("two");
-        words.add("three");
-        words.add("four");
-        words.add("five");
-        words.add("six");
-        words.add("seven");
-        words.add("eight");
-        words.add("nine");
-        words.add("ten");
+        ArrayList<word> words = new ArrayList<>();
+        words.add(new word("one","lutti"));
+        words.add(new word("two","otiiko"));
+        words.add(new word("three","tolookosu"));
+        words.add(new word("four","oyyisa"));
+        words.add(new word("five","massokka"));
+        words.add(new word("six","temmokka"));
+        words.add(new word("seven","kenekaku"));
+        words.add(new word("eight","kawinta"));
+        words.add(new word("nine","wo'e"));
+        words.add(new word("ten","na'aacha"));
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-        int i = 0;
-        while (i < words.size() ) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(i));
-            rootView.addView(wordView);
-            i++;
-        }
+        ArrayAdapter<word> itemsAdapter = new ArrayAdapter<>(this, R.layout.list_item, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
+        //LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        //int i = 0;
+        //while (i < words.size() ) {
+        //    TextView wordView = new TextView(this);
+        //    wordView.setText(words.get(i));
+        //    rootView.addView(wordView);
+        //    i++;
+        //}
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/word.java
@@ -0,0 +1,26 @@
+package com.example.android.miwok;
+
+/**
+ * Created by james on 6/12/2018.
+ */
+
+public class word {
+
+    //this is the devices default language word
+    private String mDefaultTranslation;
+    //miwok translation of the word.
+    private String mMiwokTranslation;
+
+    public word(String defaultTranslation, String miwokTranslation){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+}
